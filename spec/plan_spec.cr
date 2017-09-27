b535de74cc2ef021b1b5a2f1dc5563e3322f6626
firejox <firejox@gmail.com>
2017-09-27 12:15:10 +0800

    Make `FFTW::Plan` support DFT of real data

--- spec/plan_spec.cr
+++ spec/plan_spec.cr
@@ -5,7 +5,7 @@ r = Random.new
 describe FFTW::Plan do
   describe "1-Dimension plan" do
     plan = FFTW::Plan.new(32)
-    it "can compute DFT" do
+    it "can compute DFT of complex data" do
       arr = Array.new(32) { Complex.new(r.next_u, r.next_u) }
       dft_arr_fallback = Array.new(32) do |i|
         x = Complex.new(0, 0)
@@ -22,10 +22,37 @@ describe FFTW::Plan do
       end
     end
 
-    it "can compute inverse DFT" do
+    it "can compute inverse DFT of complex data" do
       arr = Array.new(32) { Complex.new(r.next_u, r.next_u) }
 
       arr_back = plan.idft(plan.dft(arr))
+
+      arr_back.zip(arr) do |a, b|
+        a.should be_close(b * 32, 1e-2)
+      end
+    end
+
+    it "can compute DFT of real data" do
+      arr = Array.new(32) { r.next_u.to_f }
+      dft_arr_fallback = Array.new(17) do |i|
+        x = Complex.new(0, 0)
+        arr.each_with_index do |a, j|
+          x += a * Complex.new(Math.cos(Math::PI * i * j / 16.0), -Math.sin(Math::PI * i * j / 16.0))
+        end
+        x
+      end
+
+      dft_arr = plan.dft(arr)
+
+      dft_arr.zip(dft_arr_fallback) do |a, b|
+        a.should be_close(b, 1e-2)
+      end
+    end
+
+    it "can compute inverse DFT of real data" do
+      arr = Array.new(32) { r.next_u.to_f }
+
+      arr_back = plan.idft_r(plan.dft(arr))
 
       arr_back.zip(arr) do |a, b|
         a.should be_close(b * 32, 1e-2)
@@ -35,7 +62,7 @@ describe FFTW::Plan do
 
   describe "2-Dimension plan" do
     plan = FFTW::Plan.new(8, 8)
-    it "can compute DFT" do
+    it "can compute DFT of complex data" do
       mat = Array.new(8) { Array.new(8) { Complex.new(r.next_u, r.next_u) } }
       dft_mat_fallback = Array.new(8) do |i|
         Array.new(8) do |j|
@@ -57,7 +84,7 @@ describe FFTW::Plan do
       end
     end
 
-    it "can computer inverse DFT" do
+    it "can computer inverse DFT of complex data" do
       mat = Array.new(8) { Array.new(8) { Complex.new(r.next_u, r.next_u) } }
       mat_flat = mat.flatten
 
@@ -67,11 +94,44 @@ describe FFTW::Plan do
         a.should be_close(b * 64, 1e-2)
       end
     end
+
+    it "can compute DFT of real data" do
+      mat = Array.new(8) { Array.new(8) { r.next_u.to_f } }
+      dft_mat_fallback = Array.new(8) do |i|
+        Array.new(5) do |j|
+          x = Complex.new(0, 0)
+          mat.each_with_index do |ary, k|
+            ary.each_with_index do |y, l|
+              c = i * k / 4.0 + j * l / 4.0
+              x += y * Complex.new(Math.cos(Math::PI * c), -Math.sin(Math::PI * c))
+            end
+          end
+          x
+        end
+      end
+
+      dft_mat_flat = plan.dft(mat.flatten)
+
+      dft_mat_flat.zip(dft_mat_fallback.flatten) do |a, b|
+        a.should be_close(b, 1e-2)
+      end
+    end
+
+    it "can compute inverse DFT of real data" do
+      mat = Array.new(8) { Array.new(8) { r.next_u.to_f } }
+      mat_flat = mat.flatten
+
+      mat_flat_back = plan.idft_r(plan.dft(mat_flat))
+
+      mat_flat_back.zip(mat_flat) do |a, b|
+        a.should be_close(b * 64, 1e-2)
+      end
+    end
   end
 
   describe "3-Dimension plan" do
     plan = FFTW::Plan.new(8, 8, 8)
-    it "can compute DFT" do
+    it "can compute DFT of complex data" do
       cube = Array.new(8) { Array.new(8) { Array.new(8) { Complex.new(r.next_u, r.next_u) } } }
       dft_cube_fallback = Array.new(8) do |i1|
         Array.new(8) do |i2|
@@ -99,7 +159,7 @@ describe FFTW::Plan do
       end
     end
 
-    it "can compute inverse DFT" do
+    it "can compute inverse DFT of complex data" do
       cube = Array.new(8) { Array.new(8) { Array.new(8) { Complex.new(r.next_u, r.next_u) } } }
       cube_flat = cube.flatten
 
@@ -110,11 +170,51 @@ describe FFTW::Plan do
         a.should be_close(b, 1e-2)
       end
     end
+
+    it "can compute DFT of real data" do
+      cube = Array.new(8) { Array.new(8) { Array.new(8) { r.next_u.to_f } } }
+      dft_cube_fallback = Array.new(8) do |i1|
+        Array.new(8) do |i2|
+          Array.new(5) do |i3|
+            x = Complex.new(0, 0)
+
+            cube.each_with_index do |mat, j1|
+              mat.each_with_index do |ary, j2|
+                ary.each_with_index do |y, j3|
+                  c = (i1 * j1 + i2 * j2 + i3 * j3) / 4.0
+
+                  x += y * Complex.new(Math.cos(Math::PI * c), -Math.sin(Math::PI * c))
+                end
+              end
+            end
+            x
+          end
+        end
+      end
+
+      dft_cube_flat = plan.dft(cube.flatten)
+
+      dft_cube_flat.zip(dft_cube_fallback.flatten) do |a, b|
+        a.should be_close(b, 1e-2)
+      end
+    end
+
+    it "can compute inverse DFT of real data" do
+      cube = Array.new(8) { Array.new(8) { Array.new(8) { r.next_u.to_f } } }
+      cube_flat = cube.flatten
+
+      cube_flat_back = plan.idft_r(plan.dft(cube_flat))
+
+      # Weird result
+      cube_flat.zip(cube_flat) do |a, b|
+        a.should be_close(b, 1e-2)
+      end
+    end
   end
 
   describe "High dimensional plan" do
     plan = FFTW::Plan.new(4, 4, 4, 4)
-    it "can compute DFT" do
+    it "can compute DFT of complex data" do
       hcube = Array.new(4) { Array.new(4) { Array.new(4) { Array.new(4) { Complex.new(r.next_u, r.next_u) } } } }
       dft_hcube_fallback = Array.new(4) do |i0|
         Array.new(4) do |i1|
@@ -146,11 +246,54 @@ describe FFTW::Plan do
       end
     end
 
-    it "can compute inverse DFT" do
+    it "can compute inverse DFT of complex data" do
       hcube = Array.new(4) { Array.new(4) { Array.new(4) { Array.new(4) { Complex.new(r.next_u, r.next_u) } } } }
       hcube_flat = hcube.flatten
 
       hcube_back_flat = plan.idft(plan.dft(hcube_flat))
+
+      hcube_back_flat.zip(hcube_flat) do |a, b|
+        a.should be_close(b * 256, 1e-2)
+      end
+    end
+
+    it "can compute DFT of real data" do
+      hcube = Array.new(4) { Array.new(4) { Array.new(4) { Array.new(4) { r.next_u.to_f } } } }
+      dft_hcube_fallback = Array.new(4) do |i0|
+        Array.new(4) do |i1|
+          Array.new(4) do |i2|
+            Array.new(3) do |i3|
+              x = Complex.new(0, 0)
+
+              hcube.each_with_index do |cube, j0|
+                cube.each_with_index do |mat, j1|
+                  mat.each_with_index do |ary, j2|
+                    ary.each_with_index do |y, j3|
+                      c = (i0 * j0 + i1 * j1 + i2 * j2 + i3 * j3) / 2.0
+                      x += y * Complex.new(Math.cos(Math::PI * c), -Math.sin(Math::PI * c))
+                    end
+                  end
+                end
+              end
+
+              x
+            end
+          end
+        end
+      end
+
+      dft_hcube_flat = plan.dft(hcube.flatten)
+
+      dft_hcube_flat.zip(dft_hcube_fallback.flatten) do |a, b|
+        a.should be_close(b, 1e-2)
+      end
+    end
+
+    it "can compute inverse DFT of real data" do
+      hcube = Array.new(4) { Array.new(4) { Array.new(4) { Array.new(4) { r.next_u.to_f } } } }
+      hcube_flat = hcube.flatten
+
+      hcube_back_flat = plan.idft_r(plan.dft(hcube_flat))
 
       hcube_back_flat.zip(hcube_flat) do |a, b|
         a.should be_close(b * 256, 1e-2)
--- src/fftw/plan.cr
+++ src/fftw/plan.cr
@@ -9,8 +9,11 @@ module FFTW
       {{ "FFTWPlanImpl(#{T.size}).new(*dims)".id }}
     end
 
-    abstract def dft(x)
-    abstract def idft(x)
+    abstract def dft(x : Array(Complex))
+    abstract def idft(x : Array(Complex))
+
+    abstract def dft(x : Array(Float64))
+    abstract def idft_r(x : Array(Complex))
   end
 end
 
@@ -21,14 +24,26 @@ private class FFTWPlanImpl(N) < FFTW::Plan
   @dft_c2c_plan = Pointer(Void).null
   @idft_c2c_plan = Pointer(Void).null
 
+  @dft_r2c_plan = Pointer(Void).null
+  @idft_c2r_plan = Pointer(Void).null
+
+  @r2c_size : Int32
+
   def initialize(*dims : Int32)
     n = dims.product { |x| x }
 
     @c_slice_in = Pointer(FFTW::LibFFTW::FFTWComplex).malloc(n).to_slice(n)
     @c_slice_out = Pointer(FFTW::LibFFTW::FFTWComplex).malloc(n).to_slice(n)
 
+    @r_slice_in = Slice(Float64).new(n)
+    @r_slice_out = Slice(Float64).new(n)
+    @r2c_size = n / dims.last * (dims.last/2 + 1)
+
     @dft_c2c_plan = FFTW::LibFFTW.fftw_plan_dft(N, pointerof(dims).as(Int32*), @c_slice_in, @c_slice_out, FFTW::LibFFTW::FFTW_FORWARD, FFTW::LibFFTW::FFTW_MEASURE)
     @idft_c2c_plan = FFTW::LibFFTW.fftw_plan_dft(N, pointerof(dims).as(Int32*), @c_slice_in, @c_slice_out, FFTW::LibFFTW::FFTW_BACKWARD, FFTW::LibFFTW::FFTW_MEASURE)
+
+    @dft_r2c_plan = FFTW::LibFFTW.fftw_plan_dft_r2c(N, pointerof(dims).as(Int32*), @r_slice_in, @c_slice_out, FFTW::LibFFTW::FFTW_MEASURE)
+    @idft_c2r_plan = FFTW::LibFFTW.fftw_plan_dft_c2r(N, pointerof(dims).as(Int32*), @c_slice_in, @r_slice_out, FFTW::LibFFTW::FFTW_MEASURE)
   end
 
   def dft(x : Array(Complex))
@@ -51,8 +66,29 @@ private class FFTWPlanImpl(N) < FFTW::Plan
     @c_slice_out.map { |elem| Complex.new(elem[0], elem[1]) }
   end
 
+  def dft(x : Array(Float64))
+    @r_slice_in.copy_from(x.to_unsafe, @r_slice_in.size)
+
+    FFTW::LibFFTW.fftw_execute(@dft_r2c_plan)
+
+    @c_slice_out[0, @r2c_size].map { |elem| Complex.new(elem[0], elem[1]) }
+  end
+
+  def idft_r(x : Array(Complex))
+    @r2c_size.times do |i|
+      @c_slice_in[i] = StaticArray[x[i].real, x[i].imag]
+    end
+
+    FFTW::LibFFTW.fftw_execute(@idft_c2r_plan)
+
+    @r_slice_out.to_a
+  end
+
   def finalize
     FFTW::LibFFTW.fftw_destroy_plan(@dft_c2c_plan)
     FFTW::LibFFTW.fftw_destroy_plan(@idft_c2c_plan)
+
+    FFTW::LibFFTW.fftw_destroy_plan(@dft_r2c_plan)
+    FFTW::LibFFTW.fftw_destroy_plan(@idft_c2r_plan)
   end
 end
